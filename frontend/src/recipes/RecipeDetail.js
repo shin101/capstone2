5e4e9c1d61c6d4817df4a969ac972f54c358f47d
shin101 <shinj0127@gmail.com>
2022-12-07 12:08:27 -0800

    added save button

--- frontend/src/recipes/RecipeDetail.js
+++ frontend/src/recipes/RecipeDetail.js
@@ -5,6 +5,7 @@ import RecipeApi from '../api/api';
 function RecipeDetail(){
   const { id } = useParams();
   const [recipe, setRecipe] = useState(null);
+  const [saveButton, setSaveButton] = useState(true);
 
   useEffect(() => {
     async function getDetail(){
@@ -18,6 +19,10 @@ function RecipeDetail(){
       <h2>{recipe? recipe.food_title : ""}</h2>
       <p>Servings: {recipe? recipe.servings : ""}</p>
       <p>Instruction : {recipe? recipe.instructions : ""}</p>
+      { saveButton?
+        <button onClick={()=>setSaveButton(false)}>Save</button> 
+      :
+        <button onClick={()=>setSaveButton(true)}>Unsave</button>}
 
     </div>
   );
