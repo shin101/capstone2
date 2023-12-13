28b45f942bfc673fd35218b0ebbac9a8d13e17ef
shin101 <kendras0127@gmail.com>
2023-12-13 14:56:35 -0500

    set up tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
